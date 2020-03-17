4fc139727dcf99aba5364446ebeb1781d18085c6
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-03-17 07:15:06 -0700

    attempt at creating boiler plate for index, app and components

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Posts from 'Posts.js';
+
+const App = () =>{
+    return {
+
+    }
+    
+}
+export default App;
---
+++ src/Components/Posts.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Post from 'post.js';
+
+const Posts = () => {
+    return {
+
+    }
+}
+
+export default Posts;
