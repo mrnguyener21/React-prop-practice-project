1ce470bde8c740c8e215db7aef1297b1ea32a951
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-03-18 21:06:37 -0700

    created basic files for my version of the Prop Project

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import Posts from './Components/Posts/Posts';
-import './App.css'
+import Cards from './components/Cards/Cards';
 
-const App = () =>(
-    <div className='main-container'>
-        <h1 className='Main-heading'> Breaking Weather News</h1>
-        <Posts/> 
-    </div>
-);
+const App = () =>{
+    return(
+        <div>
+            <h1>APP</h1>
+        </div>
+    )
+};
 export default App;
 
-//QUESTIONS
-// 1) WHY ARE WE NOT USING CURLY BRACKETS FOR THE App
-    // because we are only return so the curly brackets can be ommitted
-// 2) why do we not use a return
-    // -cause arrow function already has an auto return duh
-// 3) why is it import Posts from './Components/Posts/Posts' and not import Posts from './Components/Posts';
-    // cause it's in the file duh
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '..Card/Card';
+
+const Cards = () => {
+    return (
+        <div>
+            <h1>CARDS</h1>
+        </div>
+    )
+}
+
+export default Cards;
